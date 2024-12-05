c047982f9975521a7565a82be3fcd7e2fa812ed2
Novikov Artem <novikovaa6@student.bmstu.ru>
2024-12-05 15:01:29 +0300

    resolve conflict

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -29,13 +29,61 @@ void Canvas::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event);
     QPainter painter(this);
+<<<<<<< HEAD
+=======
+    // painter.setRenderHint(QPainter::Antialiasing);
+
+    Point A(-0.5, 1, 8);
+    Point B(-2.5, 1, 8);
+    Point C(-2.5, -1, 8);
+    Point D(-0.5, -1, 8);
+
+    Point E(-0.5, 1, 6);
+    Point F(-2.5, 1, 6);
+    Point G(-2.5, -1, 6);
+    Point H(-0.5, -1, 6);
+
+    std::shared_ptr<Triangle> t1 = std::make_shared<Triangle>(QVector<Point>{A, B, C}, Qt::red);
+    std::shared_ptr<Triangle> t2 = std::make_shared<Triangle>(QVector<Point>{A, C, D}, Qt::red);
+    std::shared_ptr<Triangle> t3 = std::make_shared<Triangle>(QVector<Point>{E, A, D}, Qt::green);
+    std::shared_ptr<Triangle> t4 = std::make_shared<Triangle>(QVector<Point>{E, D, H}, Qt::green);
+    std::shared_ptr<Triangle> t5 = std::make_shared<Triangle>(QVector<Point>{F, E, H}, Qt::cyan);
+    std::shared_ptr<Triangle> t6 = std::make_shared<Triangle>(QVector<Point>{F, H, G}, Qt::cyan);
+    std::shared_ptr<Triangle> t7 = std::make_shared<Triangle>(QVector<Point>{B, F, G}, Qt::yellow);
+    std::shared_ptr<Triangle> t8 = std::make_shared<Triangle>(QVector<Point>{B, F, C}, Qt::yellow);
+    std::shared_ptr<Triangle> t9 = std::make_shared<Triangle>(QVector<Point>{E, F, B}, Qt::white);
+    std::shared_ptr<Triangle> t10 = std::make_shared<Triangle>(QVector<Point>{E, B, A}, Qt::white);
+    std::shared_ptr<Triangle> t11 = std::make_shared<Triangle>(QVector<Point>{C, G, H}, Qt::blue);
+    std::shared_ptr<Triangle> t12 = std::make_shared<Triangle>(QVector<Point>{C, H, D}, Qt::blue);
+
+
+    // AddModel(t1);
+    // AddModel(t12);
+    // AddModel(t2);
+    // AddModel(t4);
+    // AddModel(t6);
+    // AddModel(t5);
+    // AddModel(t11);
+    // AddModel(t7);
+    // AddModel(t10);
+    // AddModel(t8);
+    // AddModel(t9);
+    // AddModel(t3);
+
+    // AddModel(std::make_shared<Triangle>(A, B, C, Qt::green));
+
+    AddModel(std::make_shared<CarcasModel>(GenerateShape(2, 100, 100, Qt::green, Point(0, 0, 8))));
+>>>>>>> c55a998 (add timer)
 
     AbstractStrategyProjection *strategy = new PerspectivStrategyProjection();
 
     DrawVisitor visitor(&painter, size(), strategy, light_);
 
     auto start = clock();
+<<<<<<< HEAD
 
+=======
+>>>>>>> c55a998 (add timer)
     for (auto &object : carcas_models_) {
         object->Accept(visitor);
     }
